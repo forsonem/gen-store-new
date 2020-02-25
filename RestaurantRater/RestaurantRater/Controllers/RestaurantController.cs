2b905f4ac44bcc7b30980655b497c3988144305d
forsonem <forsonem@gmail.com>
2020-02-25 15:04:33 -0500

    controllers

--- RestaurantRater/RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,6 +13,8 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : ApiController
     {
         private readonly RestaurantDbContext _db = new RestaurantDbContext();
+
+        //POST
         public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
         {
             if (ModelState.IsValid && restaurant != null)
@@ -21,11 +24,66 @@ namespace RestaurantRater.Controllers
                 return Ok();
             }
             return BadRequest(ModelState);
+        }
 
+        //GET ALL
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> allRestaurants = await _db.Restaurants.ToListAsync();
+            return Ok(allRestaurants);
+        }
+        //GetBy ID
+        public async Task<IHttpActionResult> GetByID(int id)
+        {
+            Restaurant restaurant = await _db.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+        //PUT (Update)
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody] Restaurant model)
+        {
+            
+            if(ModelState.IsValid && !(model == null))
+            {
+                //This is our entity
+                Restaurant restaurant = await _db.Restaurants.FindAsync(id);
+
+                if (restaurant != null)
+                {
+                    restaurant.Name = model.Name;
+                    restaurant.Rating = model.Rating;
+                    restaurant.Style = model.Style;
+                    restaurant.DollarSigns = model.DollarSigns;
+
+                    await _db.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
 
         }
-        
+        //DeleteByID
 
-        
+        public async Task<IHttpActionResult> DeleteRestaurant(int id)
+        {
+            Restaurant restaurant = await _db.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            _db.Restaurants.Remove(restaurant);
+
+            if(await _db.SaveChangesAsync() == 1)
+            {
+                return Ok();
+            }
+            return InternalServerError();
+        }
     }
 }
